db84b2852ca777969ba74a29d2b65d13d0690a96
Nilesh Patharkar <nilu.patharkar@gmail.com>
2022-05-16 07:18:29 +0530

    Parrallel Execution Update

--- src/main/java/TestNGRelated/TestNGTest.java
+++ src/main/java/TestNGRelated/TestNGTest.java
@@ -12,39 +12,81 @@ import org.testng.annotations.Test;
 
 public class TestNGTest {
 	
-	WebDriver driver;
+	
 	
 	@BeforeMethod
 	public void setup()
 	{
-		System.setProperty("webdriver.chrome.driver", "E:\\Practice\\chromedriver.exe");
-
+		
+		System.out.println("Starting Setup Method");
 		  //Initializing the browser driver
-		  driver = new ChromeDriver();
+		 
+		 
+	}
+	
+	@Test
+	public void actualTest1() throws InterruptedException
+	{
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver", "E:\\Practice\\chromedriver.exe");
+		driver = new ChromeDriver();
 
 		  //Navigating through a particular website
 		  driver.get("https://www.google.com");
 		  driver.manage().deleteAllCookies();
 		  driver.manage().window().maximize();
 		  driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-		 
-	}
-	
-	@Test(invocationCount = 2)
-	public void actualTest() throws InterruptedException
-	{
-	
-		 WebElement ele = driver.findElement(By.xpath("//input[@title='Search']"));
+		System.out.println("The thread ID for @Test1 Chrome is "+ Thread.currentThread().getId()); 
+		WebElement ele = driver.findElement(By.xpath("//input[@title='Search']"));
 		  ele.sendKeys("Nilesh Patharkar");
 		  Thread.sleep(1000);
-		  System.out.println("Selenium WebdriverScript in Chrome");
-		 
+		  System.out.println("Selenium WebdriverScript in Chrome - Test1");
+		  driver.close();
+	}
+	@Test
+	public void actualTest2() throws InterruptedException
+	{
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver", "E:\\Practice\\chromedriver.exe");
+		driver = new ChromeDriver();
+
+		  //Navigating through a particular website
+		  driver.get("https://www.google.com");
+		  driver.manage().deleteAllCookies();
+		  driver.manage().window().maximize();
+		  driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		System.out.println("The thread ID for @Test2 Chrome is "+ Thread.currentThread().getId());
+		WebElement ele = driver.findElement(By.xpath("//input[@title='Search']"));
+		  ele.sendKeys("Romit Patharkar");
+		  Thread.sleep(1000);
+		  System.out.println("Selenium WebdriverScript in Chrome - Test2");
+		  driver.close();
+	}
+	@Test
+	public void actualTest3() throws InterruptedException
+	{	
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver", "E:\\Practice\\chromedriver.exe");
+		driver = new ChromeDriver();
+
+		  //Navigating through a particular website
+		  driver.get("https://www.google.com");
+		  driver.manage().deleteAllCookies();
+		  driver.manage().window().maximize();
+		  driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		System.out.println("The thread ID for @Test3 Chrome is "+ Thread.currentThread().getId());
+		WebElement ele = driver.findElement(By.xpath("//input[@title='Search']"));
+		  ele.sendKeys("Manjari Patharkar");
+		  Thread.sleep(1000);
+		  System.out.println("Selenium WebdriverScript in Chrome  - Test3");
+		  driver.close();
 	}
 	
 	@AfterMethod
 	public void tearDown()
 	{
-		 driver.close();
+		System.out.println("Ending Setup Method");
+
 	}
 	
 
